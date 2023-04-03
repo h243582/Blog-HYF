8c136908576e2d01fe649201d0d3688e8baa1f12
heyufei <l-heyufei@qianxin.com>
2023-04-03 22:04:51 +0800

    11

---
+++ blog-platform/blog-storage/src/main/java/com/heyufei/storage/channel/ChannelFactory.java
@@ -0,0 +1,31 @@
+package com.heyufei.storage.channel;
+
+
+import com.heyufei.common.common.SpringContextUtils;
+import com.heyufei.common.constant.UploadConstants;
+
+/**
+ * 文件上传方式
+ *
+ * @author: Naccl
+ * @date: 2022-01-23
+ */
+public class ChannelFactory {
+	/**
+	 * 创建文件上传方式
+	 *
+	 * @param channelName 方式名称
+	 * @return 文件上传Channel
+	 */
+	public static FileUploadChannel getChannel(String channelName) {
+		switch (channelName.toLowerCase()) {
+			case UploadConstants.LOCAL:
+				return SpringContextUtils.getBean(LocalChannel.class);
+			case UploadConstants.GITHUB:
+				return SpringContextUtils.getBean(GithubChannel.class);
+			case UploadConstants.UPYUN:
+				return SpringContextUtils.getBean(UpyunChannel.class);
+		}
+		throw new RuntimeException("Unsupported value in [application.properties]: [upload.channel]");
+	}
+}
